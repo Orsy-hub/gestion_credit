46ba1b8a0259ac4a80a1104bd884265b323e5de8
Raven <jazzysamakami@gmail.com>
2025-02-22 11:48:39 +0100

    Mise en place de la restriction de la connexion. Seul les utilisateur dont la demande d'inscription ont ete valide par l'admin peuvent desormais s'inscrire

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,6 +32,13 @@
             </label>
         </div>
 
+        <div class="mt-4 text-blue-500 font-ubuntu font-medium">
+            <!-- Affichage de l'erreur 'verifier' -->
+            @error('verifier')
+                <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
